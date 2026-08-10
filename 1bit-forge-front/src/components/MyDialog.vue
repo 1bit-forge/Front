65570c5ced3b7443eb3357be06f2ba575a96a954
Ivan Pun <ivan@insys.com.mo>
2026-08-10 17:03:36 +0800

    setupMyDialog

---
+++ 1bit-forge-front/src/components/MyDialog.vue
@@ -0,0 +1,21 @@
+<template>
+    <el-dialog v-model="dialogVisible" title="Tips" width="500" :before-close="handleClose">
+        <slot></slot>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="dialogVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="dialogVisible = false">
+                    Confirm
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
+</template>
+
+<script setup>
+const props = defineEmits({
+    
+})
+</script>
+
+<style scoped></style>
