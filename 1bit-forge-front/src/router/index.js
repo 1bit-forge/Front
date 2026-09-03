52a19b902089d2a7f79f185b93a2671076e1df02
Ivan Pun <ivan@insys.com.mo>
2026-09-03 12:59:24 +0800

    Change frontend to hash mode

--- 1bit-forge-front/src/router/index.js
+++ 1bit-forge-front/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import MainLayout from '@/layouts/MainLayout.vue'
 import CalendarView from '@/views/CalendarView.vue'
 import SettingView from '@/views/SettingView.vue'
@@ -7,7 +7,7 @@ import RegisterView from '@/views/RegisterView.vue'
 import { isAuthenticated } from '@/composables/useAuth'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/login',
